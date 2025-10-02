4cc260e27eeff1a8802feff61499a7008a8314dd
satoshi-sh <satoss1108@gmail.com>
2025-10-02 18:56:42 -0500

    Finished ep16

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +8,10 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+  <RouterLink to="/contact">Contact</RouterLink>
+  <RouterView/>
 </template>
 
 <style scoped></style>
