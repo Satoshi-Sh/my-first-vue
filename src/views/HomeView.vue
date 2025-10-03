01184193791398d696d225cdbde87cb9618366b3
satoshi-sh <satoss1108@gmail.com>
2025-10-03 12:39:23 -0500

    Finished ep19

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+let message =ref("Hello world");
+setTimeout(()=>{
+  message.value="I have been changed"
+},2000);
+
+const doSomething = ()=> {
+  alert("Hello there")
+}
+ </script>
+<template>
+  <h2>Home</h2>
+  <p>{{message}}</p>
+  <button @click="doSomething">Click me</button>
+  <p>
+    <input v-model="message" type="text"/>
+  </p>
+</template>
