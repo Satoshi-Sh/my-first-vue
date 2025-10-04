a0a818a886736f390c08bf9674113fd88a3703fd
satoshi-sh <satoss1108@gmail.com>
2025-10-04 11:52:08 -0500

    Finished ep 23

---
+++ src/components/Quiz/QuizFooter.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { inject } from "vue";
+import QuizFooterLinks from "@/components/Quiz/QuizFooterLinks.vue";
+
+defineProps({
+  quiz: Object
+});
+
+</script>
+
+<template>
+  <div>
+    <h5>{{quiz.name}}</h5>
+  </div>
+
+  <footer>
+    <h4>Quiz Footer</h4>
+    <QuizFooterLinks :quiz="quiz" />
+  </footer>
+</template>
